7ea05a82c0ce38f0b4c6453c769a0e9d04a41d94
ewoutvdb <vdbewout@gmail.com>
2022-08-10 14:00:20 +0200

    cursor

--- src/component/input_line.rs
+++ src/component/input_line.rs
@@ -1,6 +1,10 @@
+use std::{cmp};
+
 use crossterm::event::{KeyCode, KeyEvent};
 use tui::{
     layout::Alignment,
+    style::{Color, Style},
+    text::{Span, Spans},
     widgets::{Paragraph, StatefulWidget, Widget, Wrap},
 };
 
@@ -18,6 +22,7 @@ pub struct InputLineState {
     active: bool,
     theme: GlobalTheme,
     input_mode: InputMode,
+    cursor_offset: usize,
 }
 
 #[derive(Debug)]
@@ -36,15 +41,18 @@ impl InputLineState {
             theme,
             active: false,
             input_mode: InputMode::Normal,
+            cursor_offset: 0,
         }
     }
 
     pub fn clear(&mut self) {
         self.value.clear();
+        self.cursor_offset = 0;
     }
 
     pub fn set_input_mode(&mut self, mode: InputMode) {
-        self.input_mode = mode
+        self.input_mode = mode;
+        self.cursor_offset = 0;
     }
 }
 
@@ -60,10 +68,48 @@ impl StatefulWidget for InputLine {
         buf: &mut tui::buffer::Buffer,
         state: &mut Self::State,
     ) {
-        Paragraph::new(state.value.clone())
+        //let spans = Spans::from(if state.cursor_offset != 0 {
+        //    vec![Span::raw(state.value.as_str()),
+        //        Span::styled("jos", Style::default().bg(Color::White))
+
+        //    ]
+        //} else {
+        //    vec![
+        //        Span::styled(state.value.as_str(), Style::default()),
+        //        Span::styled(" ", Style::default().bg(Color::White))
+        //    ]
+        //});
+        let spans = match (state.input_mode, state.cursor_offset) {
+            (InputMode::Normal, _) => Spans::from(Span::raw(state.value.as_str())),
+            //(InputMode::Editing, 0) => Spans::from(vec![
+            //    Span::raw(state.value.as_str()),
+            //    Span::styled(" ", Style::default().bg(Color::White)),
+            //]),
+            (InputMode::Editing, 0) => {
+                Spans::from(vec![
+                    Span::raw(&state.value),
+                    Span::styled(" ", Style::default().bg(Color::White)),
+                ])
+            },
+            (InputMode::Editing, cursor_offset) => {
+                let offset = std::cmp::max(state.value.len() - cursor_offset, 0);
+                let left = &state.value[..offset];
+                let cursor = &state.value[offset..=offset];
+                let right = &state.value[offset+1..];
+
+                Spans::from(vec![
+                    Span::raw(left),
+                    Span::styled(cursor, Style::default().bg(Color::White)),
+                    Span::raw(right),
+                ])
+            },
+                
+        };
+
+        Paragraph::new(spans)
             .style(state.theme.hostname(state.input_mode()))
             .alignment(Alignment::Left)
-            .wrap(Wrap { trim: true })
+            //.wrap(Wrap { trim: false }) // NOTE: Removes trailing space even with trim off
             .render(area, buf);
     }
 }
@@ -78,8 +124,21 @@ impl Component for InputLineState {
             }
             KeyCode::Backspace => {
                 // TODO: maybe a beep sound or flast when this erorrs
-                _ = self.value.pop().is_some();
+                if self.cursor_offset == 0 {
+                    _ = self.value.pop().is_some();
+                } else {
+                    _ = self.value.remove(self.value.len() - self.cursor_offset);
+                }
+                
                 Some(Action::InputResult(InputResult::Changed))
+            }
+            KeyCode::Delete => {
+                if self.cursor_offset != 0 {
+                    _ = self.value.remove(self.value.len() - self.cursor_offset + 1);
+                    Some(Action::InputResult(InputResult::Changed))
+                } else {
+                    Some(Action::InputResult(InputResult::NOOP))
+                }
             }
             KeyCode::Esc => {
                 self.value = self.prev_value.clone();
@@ -87,8 +146,29 @@ impl Component for InputLineState {
                 Some(Action::InputResult(InputResult::Canceled))
             }
             KeyCode::Char(char) => {
-                self.value.push(char);
+                if self.cursor_offset == 0 {
+                    self.value.push(char);
+                } else {
+                    self.value.insert(self.value.len() - self.cursor_offset, char)
+                }
                 Some(Action::InputResult(InputResult::Changed))
+            }
+            KeyCode::Left => {
+                if self.cursor_offset < self.value.len() {
+                    self.cursor_offset += 1;
+                    Some(Action::InputResult(InputResult::Changed))
+                } else {
+                    Some(Action::InputResult(InputResult::NOOP))
+                }
+            }
+            KeyCode::Right => {
+                // TODO: move Some(Action::...) into the match
+                if self.cursor_offset > 0 {
+                    self.cursor_offset -= 1;
+                    Some(Action::InputResult(InputResult::Changed))
+                } else {
+                    Some(Action::InputResult(InputResult::NOOP))
+                }
             }
             _ => Some(Action::InputResult(InputResult::NOOP)),
         }

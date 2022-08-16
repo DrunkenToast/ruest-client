8a50d8a104dbf55bbb5578ceed1f87b56e0af2e9
Dries1234 <driesolbrechts@gmail.com>
2022-08-16 17:57:20 +0200

    updates to http function and test

--- src/http.rs
+++ src/http.rs
@@ -3,6 +3,7 @@ use reqwest::{
     Body, IntoUrl, Method,
 };
 
+
 pub async fn http_request<T, U>(
     method: reqwest::Method,
     uri: U,
@@ -15,30 +16,43 @@ where
     T: Into<Body> + Default,
 {
     let client = reqwest::Client::new();
-    let response = match method {
-        Method::GET => {
-            client
-                .get(uri)
-                .body(body)
-                .headers(headers)
-                .header("content-type", content_type)
-                .send()
-                .await?
-        }
-        Method::POST => todo!(),
-        Method::PUT => todo!(),
-        Method::DELETE => todo!(),
-        Method::PATCH => todo!(),
-        Method::OPTIONS => todo!(),
-        Method::HEAD => todo!(),
+    let request = match method {
+        Method::GET => client.get(uri),
+        Method::POST => client.post(uri),
+        Method::PUT => client.put(uri),
+        Method::DELETE => client.delete(uri),
+        Method::PATCH => client.patch(uri),
+        Method::HEAD => client.head(uri),
         _ => todo!(),
     };
+    let request = request
+        .body(body)
+        .headers(headers)
+        .header("content-type", content_type);
+    let response = request.send().await?;
     Ok(response)
 }
 
-// usage
-// let response = http_request(reqwest::Method::GET,
-//     "https://jsonplaceholder.typicode.com/todos/1",
-//     HeaderMap::try_from(&empty).unwrap(), reqwest::header::HeaderValue::from_str("application/json").unwrap(),
-//     "{}").await?;
-// println!("{}", response.text().await?);= http_request(http::HttpMethod::GET, "https://jsonplaceholder.typicode.com/todos/1", "{}").unwrap();
+#[cfg(test)]
+mod tests {
+    use reqwest::header::{HeaderMap, HeaderValue};
+    use tokio::test;
+    use mockito::{self, mock};
+    
+    use super::http_request;
+
+    #[test]
+    async fn get_ok() {
+    let mock = mock("GET", "/").create();
+        let resp = http_request(
+            reqwest::Method::GET,
+            mockito::server_url(), 
+           HeaderMap::new(), 
+            HeaderValue::from_str("").unwrap(),
+            "{}",
+        )
+        .await.unwrap();
+        assert_eq!(resp.status(), 200)
+    }
+}
+
